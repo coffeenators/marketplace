2a36ce2061677dbeb5e406893c59982422445f44
Sarala Buradagunta <sarala.buradagunta@mastercard.com>
2015-12-05 19:01:51 -0800

    clean this shit up

--- src/main/java/com/msquared/JerseyConfig.java
+++ src/main/java/com/msquared/JerseyConfig.java
@@ -9,6 +9,8 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(HitchARide.class);
         register(CleanMyHouse.class);
+        register(FeedMe.class);
+        register(Groceries.class);
     }
 
 }
